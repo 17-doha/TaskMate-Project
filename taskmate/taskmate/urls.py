f2043a5a4a1521f5309c5ea37784b52b005f8971
mariamelghandoor <mariamyasserelghandoor@gmail.com>
2024-11-25 00:44:23 +0200

    url fix

--- taskmate/taskmate/urls.py
+++ taskmate/taskmate/urls.py
@@ -25,9 +25,8 @@ urlpatterns = [
     path('main/', views.main, name='main'),
     path('accounts/', include('allauth.urls')),
     path("", include("users.urls")),
-    path("environment/", include("environment.urls"))
-     
-
+    path("environment/", include("environment.urls")),
+    
     path("task/", include("task.urls")),
      
 ]
