ce4920824057464d3044271c9b356ec73d13ec26
Doha <doha.hemdan7@gmail.com>
2024-11-28 17:48:09 +0200

    adding table model and enviroment model

--- taskmate/environment/models.py
+++ taskmate/environment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from users.models import Login
+class Environment(models.Model):
+    environment_id = models.AutoField(primary_key=True)
+    label = models.CharField(max_length=255)
+    is_private = models.BooleanField(default=False)
+    admin_login = models.ForeignKey(Login, on_delete=models.CASCADE, related_name='admin_environments')
 
-# Create your models here.
+    def __str__(self):
+        return self.label
+
+
+class Table(models.Model):
+    table_id = models.AutoField(primary_key=True)
+    label = models.CharField(max_length=255)
+    environment = models.ForeignKey(Environment, on_delete=models.CASCADE, related_name='tables')
+    user_login = models.ForeignKey(Login, on_delete=models.CASCADE, related_name='user_tables')
+
+    def __str__(self):
+        return self.label
