f3e2c28b40a0c389450cd36db0e7502cac05018f
Doha <doha.hemdan7@gmail.com>
2024-11-29 11:21:43 +0200

    cleaned the drag and drop code

--- taskmate/environment/models.py
+++ taskmate/environment/models.py
@@ -3,25 +3,24 @@ from django.contrib.auth.models import User
 from users.models import Login
 
 class Environment(models.Model):
-    environment_id = models.AutoField(primary_key=True)  # identifier for the environment
-    label = models.CharField(max_length=255, unique=True)  # Environment name
-    is_private = models.BooleanField(default=True)  # Determines if the environment is shareable
+    environment_id = models.AutoField(primary_key=True) 
+    label = models.CharField(max_length=255, unique=True)  
+    is_private = models.BooleanField(default=True)  
     admin = models.ForeignKey(
         Login,
         related_name='admin_environments',
         on_delete=models.CASCADE
     )  # referencing the User 
 
+    class Meta:
+        db_table = 'environment'  
+
+
 class Table(models.Model):
     table_id = models.AutoField(primary_key=True)
     label = models.CharField(max_length=255)
     environment = models.ForeignKey(Environment, on_delete=models.CASCADE, related_name='tables')
     user_login = models.ForeignKey(Login, on_delete=models.CASCADE, related_name='user_tables')
-
-
-
-
-    # String representation for easier debugging
-
-    def __str__(self):
-        return self.label
+    
+    class Meta:
+        db_table = 'table'  
---
+++ taskmate/environment/static/environment.js
@@ -0,0 +1,34 @@
+const environment_id = "{{ environment.environment_id }}";
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var task = document.getElementById(data);
+    ev.target.appendChild(task);
+    var taskId = task.getAttribute('data-task-id');
+    var targetColumn = ev.target.closest('.column');
+    var targetTableName = targetColumn.querySelector('h3').textContent;
+
+    // Make a POST request to update the task's table ID
+    fetch(`/environment/${environment_id}/drag-and-drop/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+        },
+        body: JSON.stringify({
+            'task_id': taskId,
+            'target_table': targetTableName
+        })
+    })
+    .then(response => response.json())
+
+}
