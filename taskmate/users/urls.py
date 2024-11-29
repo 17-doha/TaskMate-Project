a95ba5a1aaf7478f31f741331660b25ae7dc68cd
Doha <doha.hemdan7@gmail.com>
2024-11-30 01:00:21 +0200

    done if user account with google not exist create one

--- taskmate/users/urls.py
+++ taskmate/users/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from users import views
+from . import views
 
 urlpatterns = [
-
-     path('login/', views.login_user, name = 'login'),
-     path('main/', views.main, name = 'main'),
-     path('accounts/profile/', views.custom_redirect_view), 
-     path("", views.main),
-
-
+    path('google-sign-in-callback/', views.google_sign_in_callback, name='google_sign_in_callback'),
+    path('login/', views.login_user, name='login'),
+    path('main/', views.main, name='main'),
+    path("", views.main, name='home'),
 ]
--- taskmate/users/views.py
+++ taskmate/users/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
 from .models import Login
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import check_password
+from allauth.socialaccount.models import SocialAccount
+from django.contrib.auth.models import User
+
 
 
 def login_user(request):
-
+    """
+    Handles local sign-in and ensures the user exists.
+    """
     if request.method == 'POST':
         # Get email and password from the POST request
         email = request.POST.get('email')
@@ -40,6 +45,32 @@ def custom_redirect_view(request):
 def main(request):
     # This is the page to redirect to after login
     return render(request, 'main.html')
+
+
+
+def google_sign_in_callback(request):
+    """
+    Handles Google sign-in and ensures the user exists and if not will create one.
+    """
+    if request.user.is_authenticated:
+        # Check if user exists in the custom Login model
+        social_account = SocialAccount.objects.filter(user=request.user, provider="google").first()
+        
+        if social_account:
+            email = social_account.extra_data.get("email")
+
+            try:
+                Login.objects.get(email=email)
+            except Login.DoesNotExist:
+                Login.objects.create(
+                    email=email,
+                    name=request.user.username,
+                    password="",  # Leave password empty as this is Google sign-in
+                )
+
+        return redirect("main")
+
+    return redirect("login")
 
 
 
