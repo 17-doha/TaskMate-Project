6011c41009ac5b18ab4387c2cd438edbef93fa8a
Doha <doha.hemdan7@gmail.com>
2024-12-14 18:21:07 +0200

    back end for the upcoming deadlines part in the main page

--- taskmate/main/views.py
+++ taskmate/main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.utils.timezone import now  # For getting the current date and time
+from django.db.models import Q
+from task.models import Task  # Import your Table model
 
 
 def mainpage(request, user_id):
+    # user_id = request.session.get('user_id') #"Hash this to access the user_id in any other view"
     # This is the page to redirect to after login
     """
     Renders the main dashboard page after a successful login.
@@ -16,5 +20,14 @@ def mainpage(request, user_id):
     Outputs:
     - Renders the 'mainpage.html' template.
     """
-    return render(request, 'mainpage.html')
+    current_datetime = now()
+
+    # Fetch tasks for the user with future deadlines, ordered by the nearest deadline first
+    tasks = Task.objects.filter(
+        Q(assigned_to=user_id) | Q(created_by=user_id),
+        deadline__gt=current_datetime
+    ).order_by('deadline')[:4]
+    # Pass tasks to the template
+    return render(request, 'mainpage.html', {'tasks': tasks})
+
 # Create your views here.
