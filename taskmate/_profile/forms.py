8ace48d1df06b3aab3dbaa0639c71d26cfccf182
mariamelghandoor <mariamyasserelghandoor@gmail.com>
2024-12-19 17:05:31 +0200

    all edit done till notice tanya

--- taskmate/_profile/forms.py
+++ taskmate/_profile/forms.py
@@ -4,8 +4,10 @@ from signup.models import User
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name']
+        fields = ['first_name', 'last_name','phone_number','age']
 
     first_name = forms.CharField(required=False)
     last_name = forms.CharField(required=False)
+    phone_number = forms.CharField(required=False)
+    age = forms.CharField(required=False)
     
